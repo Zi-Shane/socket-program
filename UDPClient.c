58f7aeaeeb5ae1c86bf93bbdd1089784308f13f2
Zi-Shane <shane110319@gmail.com>
2019-10-01 12:54:47 +0800

    UDP part complete

--- UDPClient.c
+++ UDPClient.c
@@ -10,15 +10,16 @@
 #define Server_PortNumber 5555
 #define Server_Address "192.168.161.137"
 
-int main() {
+int main()
+{
     struct sockaddr_in address;
     int sock, byte_sent;
-    char buffer[100]="hello\0";
-    scanf("%[^\n]",buffer);
+    char buffer[100] = "hello\0";
 
     // create socket
     sock = socket(PF_INET, SOCK_DGRAM, 0);
-    if(sock < 0) printf("Error creating socker\n");
+    if (sock < 0)
+        printf("Error creating socker\n");
     // fillin struct
     bzero(&address, sizeof(address));
     address.sin_family = AF_INET;
@@ -26,10 +27,22 @@ int main() {
     address.sin_addr.s_addr = inet_addr(Server_Address);
 
     int address_length = sizeof(address);
-    // sent packet
-    byte_sent = sendto(sock, buffer, sizeof(buffer), 
-        0, (struct sockaddr *)&address, address_length);
+    
+    
+    // send packet
+    printf("Packet content: ");
+    scanf("%[^\n]", buffer);
+    byte_sent = sendto(sock, buffer, sizeof(buffer),
+                       0, (struct sockaddr *)&address, address_length);
     if (byte_sent < 0) printf("Error sending packet\n");
+
+    //receive
+    int byte_recv, client_address, client_address_length;
+    char buffer2[100] = "";
+    byte_recv = recvfrom(sock, buffer2, sizeof(buffer),
+                         0, (struct sockaddr *)&client_address, &client_address_length);
+    printf("%s\n", buffer2);
+    if (byte_recv < 0) printf("Error recving packet\n");
 
     close(sock);
     return 0;
--- UDPServer.c
+++ UDPServer.c
@@ -9,34 +9,43 @@
 
 #define PortNumber 5555
 
-int main() {
+int main()
+{
     struct sockaddr_in address, client_address;
     int sock, byte_recv, client_address_length;
     char buffer[50];
 
     // create socket
     sock = socket(PF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) printf("Error creating socket\n");
+    if (sock < 0)
+        printf("Error creating socket\n");
     // init
     address.sin_family = AF_INET;
     address.sin_port = htons(PortNumber);
     address.sin_addr.s_addr = INADDR_ANY;
     // bind
-    if(bind(sock, (struct sockaddr *)&address, sizeof(address)) == -1) {
+    if (bind(sock, (struct sockaddr *)&address, sizeof(address)) == -1)
+    {
         printf("Error binding\n");
         close(sock);
     }
-    
+
     int address_length = sizeof(address);
-    // 
+    // wating for client
     while (1)
     {
-        byte_recv = recvfrom(sock, buffer, sizeof(buffer), 
-            0, (struct sockaddr *)&client_address, &client_address_length);
+        // receive packet
+        byte_recv = recvfrom(sock, buffer, sizeof(buffer),
+                             0, (struct sockaddr *)&client_address, &client_address_length);
         if (byte_recv < 0) printf("Error recving packet\n");
-        printf("data: %s\n", buffer);
-    }
+        printf("Received packet: %s\n", buffer);
 
+        // response "welcome"
+        char buffer2[50] = "welcome\0";
+        int byte_sent = sendto(sock, buffer2, sizeof(buffer2),
+                               0, (struct sockaddr *)&client_address, client_address_length);
+        if (byte_sent < 0) printf("Error sending packet\n");
+    }
 
     return 0;
 }
